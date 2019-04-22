d6955324e5fe324366dee1745807b7e2509e6bd2
Thomas-Dimos <tomdimos55@gmail.com>
2019-04-22 14:08:17 +0300

    Final commit.Events have been merged and tested

--- src/controllers/Event.controller.js
+++ src/controllers/Event.controller.js
@@ -2,36 +2,50 @@ const User = require('../models/User.model');
 const mongoose = require('mongoose');
 
 
-exports.getEvents = function(req,res,next) {
+exports.getEvents = function(req,res) {
     const userID = mongoose.Types.ObjectId(res.userID);
-    if(req.path === 'Event/QR'){
-        User.find({"_id": userID},{"Events.eventType" : "QREvent"}).exec(function (err,qrEvents){
-            if(err){
-                res.status(500).send('Unexpected Internal Error');
-                return;
-            }
-            res.send(JSON.stringify(qrEvents,null,'\t'));
-        });
-    }else if (req.path === 'Event/Beacon'){
-        User.find({"_id": userID},{"Events.eventType" : "BeaconEvent"}).exec(function (err,beaconEvents){
-            if(err){
-                res.status(500).send('Couldn\'t find user');
-                return;
-            }
-            res.send(JSON.stringify(beaconEvents,null,'\t'));
-            
-        });
+    let eventType;
+    if(req.path === '/Events/QR'){
+        eventType = 'QREvent';
+    }else{
+        eventType = 'BeaconEvent';
     }
+    User.aggregate([
+        {
+            "$match": {"_id": userID}
+        },
+            {
+            "$project": {
+                "Events": {
+                    "$filter": {
+                        input: "$Events",
+                        as: "event",
+                        cond: {"$eq": ["$$event.eventType", eventType]}
+                    }
+                }
+            }
+        }
+    ]).exec(function (err,result){
+    if(err){
+        console.log(err);
+        res.status(500).send('Unexpected Internal Error');
+        return;
+    }
+    res.send(JSON.stringify(result[0].Events,null,'\t'));
+});
+   
 }
 
 exports.index = function(req,res) {
-    User.findOne({'_id': res.userID},'Events'
-    ).exec(function (err,events){
+    const userID = mongoose.Types.ObjectId(res.userID);
+    User.findOne({'_id': userID}
+    ).exec(function (err,result){
         if(err){
             res.status(500).send('Unexpected Internal Error');
             return;
         }
-        res.send(JSON.stringify(events,null,'\t'));
+
+        res.send(JSON.stringify(result.Events,null,'\t'));
     });
 }
 
@@ -66,32 +80,33 @@ exports.new = function(req,res) {
 
 exports.newSync = function(req,res) {
     const date = new Date(req.body.timeStamp);
-    User.update({"_id" : res.userID},{
-        "$push": {
+    date.setHours((date.getHours()+2));
+    User.updateOne(
+        {"_id" : res.userID},
+        {"$push": {
             "Events": {
-                timeStamp: date,
-                location: {
-                    speed: req.body.location.speed,
-                    heading: req.body.location.heading,
-                    accuracy: req.body.location.accuracy,
-                    altitude: req.body.location.altitude,
-                    longitude: req.body.location.longitude,
-                    latitude: req.body.location.latitude
-                },
-                eventType: req.body.eventType,
-                data: req.body.data,
-                "&sort": {timeStamp: -1}
-            }
+                "$each": [{timeStamp: date,
+                    location: {
+                        speed: req.body.location.speed,
+                        heading: req.body.location.heading,
+                        accuracy: req.body.location.accuracy,
+                        altitude: req.body.location.altitude,
+                        longitude: req.body.location.longitude,
+                        latitude: req.body.location.latitude
+                    },
+                    "eventType": req.body.eventType,
+                    "data": req.body.data}],
+                "$sort": {timeStamp: 1}
+            },
+            
         }
-    })
-    .exec().then(function(user){
-        user.save((err) => {
-            if(err){
-                console.log(err);
-                res.status(500).send('Unable to save to Database');
-                return;
-            }
+    },function(err,doc){
+        if(err){
+            console.log(err);
+            res.status(500).send('Unable to save to Database');
+            return;
+        }else{
             res.send('Event successfully stored in database');
-        });
+        }
     });
 }
